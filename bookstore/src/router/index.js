c203ad10267d86f840cad6cdd8d24004feb19c1d
rayrayrayrayray5 <raymondl187@nycstudents.net>
2026-06-05 13:40:46 -0400

    wait

--- bookstore/src/router/index.js
+++ bookstore/src/router/index.js
@@ -1,20 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../views/LoginView.vue'
 import RegisterView from '../views/RegisterView.vue'
+import Store from '../views/TheyCallItTheStore.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-    path: '/login',
-    name: 'login',
-    component: LoginView,
+      path: '/login',
+      name: 'login',
+      component: LoginView,
     },
 
     {
-    path: '/register',
-    name: 'register',
-    component: RegisterView,
+      path: '/register',
+      name: 'register',
+      component: RegisterView,
     },
     {
       path: '/about',
@@ -24,6 +25,11 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    /*     {
+      path: '/store',
+      name: 'store',
+      component: Store,
+    }, */
   ],
 })
 
