af58ab40332dd929ebea1d8e1e916e5ee67ef3b3
rayrayrayrayray5 <raymondl187@nycstudents.net>
2026-06-11 14:01:22 -0400

    weiming you broke the code

--- bookstore/src/lib/supabase.js
+++ bookstore/src/lib/supabase.js
@@ -3,7 +3,4 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = import.meta.env.VITE_Supabase_URL
 const supabaseKey = import.meta.env.VITE_Supabase_Key
 
-export const supabase = createClient(
- supabaseUrl,
- supabaseKey
-)
+export const supabase = createClient(supabaseUrl, supabaseKey)
